8d25f0e5c80cce00a222fb9d727cd0d4474d496d
jordi-olivares <jordicuenta2@gmail.com>
2022-09-28 21:29:52 -0500

    alerts, toast y modal simple

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AlertController, AlertOptions, IonModal, ToastController, ToastOptions } from '@ionic/angular';
+//import { listenerCount } from 'process';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,62 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private alertController: AlertController,private toastController: ToastController) {}
+  async showAlert(){
+    const options:AlertOptions={
+      header:'Cerrar Session',
+      subHeader:'important mensage',
+      message:'This is an alert',
+      buttons:[{
+        text: 'si',
+        handler:()=>{console.log('si')}
+      },
+      {
+        text: 'no',
+        role: 'cancel',
+        handler:()=>{console.log('NO')}
+      }
+    ]
+    }
+    
+    const alert=await this.alertController.create(options);
+    alert.present();
+  }
+
+  async showToast(position:'top'|'middle'|'bottom'){
+    const options:ToastOptions={
+      position,
+      message:'Se cerro la sesion correctamenete',
+      duration:1500,
+      buttons:[{
+        text: 'OK',
+        handler:()=>{console.log('Ok')}
+      }
+    ]
+    }
+    
+    const toast=await this.toastController.create(options);
+    toast.present();
+  }
+
+  @ViewChild(IonModal) modal: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 
 }
